114c55abafe509b3c5708cc7f46a6c2c6f40fb6b
mcianni <mcianni@gmail.com>
2013-12-30 19:11:33 -0500

    purty up the home page

--- app/assets/javascripts/goals.js.coffee
+++ app/assets/javascripts/goals.js.coffee
@@ -7,9 +7,16 @@ $ ->
   $("#goal_description").val($("#goal_description").val())
 
   $("#goal_description").keyup (e) ->
+    $("span.quantity").removeClass("hidden")
     matches = $("#goal_description").val().match(/\d+/g)
     if matches && matches.length == 1
       $("span.quantity").html(matches[0])
+      $("span.quantity").removeClass("label-danger")
+      $("span.quantity").addClass("label-success")
+      $("input[type='submit']").attr("disabled", null)
     else
       $("span.quantity").html("X")
+      $("span.quantity").removeClass("label-success")
+      $("span.quantity").addClass("label-danger")
+      $("input[type='submit']").attr("disabled", "disabled")
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,36 @@
 class PagesController < ApplicationController
 
   def home
+    @examples = [
+      'run x miles',
+      'take my dog hiking x times',
+      'do x good deeds',
+      'will go to the gym x times',
+      'save x dollars',
+      'use x dollars worth of coupons',
+      'watch x documentaries',
+      'read x books',
+      'read x pages of books',
+      'paint x paintings',
+      'write in my diary x times',
+      'volunteer x times',
+      'cook x meals for others',
+      'see live entertainment x times',
+      'host x parties',
+      'bikes x miles',
+      'swim x laps',
+      'bike to work x times',
+      'write x songs',
+      'sell x things from my overfilling attic',
+      'opt to shop locally instead of at a big box store x times',
+      'cook instead of eating out x times',
+      'donate to charity x times',
+      'write x hand written letters',
+      'contribute to x open source projects',
+      'lose x pounds',
+      'contribute to x kickstarter projects',
+      'buy my wife/husband flowers x times'
+    ]
   end
 
 end
